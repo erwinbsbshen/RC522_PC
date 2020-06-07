3efc0cee7738665f06eaafdbe6f6569ea1beb33a
zhu7116 <3492152735@qq.com>
2020-06-07 15:40:00 +0800

    main edit

--- code/main.c
+++ code/main.c
@@ -97,7 +97,7 @@ void main(void)
     lcdInitinal();
     lcdMsg("浙江中医药大学",1,0);
 	lcdMsg("嵌入式课程",2,0);
-	lcdMsg("XXX",3,0);
+	lcdMsg("YYY",3,0);
     lcdMsg("www.zcmu.edu.cn",4,0);
     delay_ms(2000);
     lcdInitinal();
