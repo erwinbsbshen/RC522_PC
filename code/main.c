dafd0ed6d15179fa2bbee36cdfd2aae997c5ceac
RoiLongo <645249561@qq.com>
2020-06-12 17:51:07 +0800

    main.edit

--- code/main.c
+++ code/main.c
@@ -95,10 +95,10 @@ void main(void)
     }
 
     lcdInitinal();
-    lcdMsg("浙江中医药大学",1,0);
-	lcdMsg("嵌入式课程",2,0);
+    lcdMsg("王星龙",1,0);
+	lcdMsg("浙江中医药大学",2,0);
 	lcdMsg("XXX",3,0);
-    lcdMsg("www.zcmu.edu.cn",4,0);
+    lcdMsg("www.google.com",4,0);
     delay_ms(2000);
     lcdInitinal();
     delay_ms(2);
